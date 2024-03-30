89136b0a75b2423a08b084c8627a84a30cf543bf
M.PeTKoVv <petkov.martin19@gmail.com>
2024-03-30 21:32:25 +0200

    Configure Categories

---
+++ ToDoApp.Data/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace ToDoApp.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using ToDoApp.Data.Models;
+
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+
+        }
+
+        private Category[] GenerateCategories()
+        {
+            ICollection<Category> categories = new HashSet<Category>();
+
+            Category category;
+
+            category = new Category
+            {
+                Id = 1,
+                Name = "Personal",
+                ImageUrl = ""
+            };
+            categories.Add(category);
+
+            return categories.ToArray();
+        }
+    }
+}
